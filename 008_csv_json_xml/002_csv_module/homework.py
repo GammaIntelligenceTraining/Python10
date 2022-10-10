1526439ce82fa144a0bdee8c8f621dac4af9eb07
Roman Kutselepa <roman.kutselepa@gmail.com>
2022-10-10 18:34:58 +0300

    Samples regexp

--- 008_csv_json_xml/002_csv_module/homework.py
+++ 008_csv_json_xml/002_csv_module/homework.py
@@ -1,34 +1,40 @@
-import csv
-
-def sort_value(some_list):
-    return (sorted(some_list, key=lambda x: x[2], reverse=True))
-
-data_list = []
-with open('csv_files//2019.csv', 'r', encoding='UTF8') as csv_file:
-    # next(csv_file)
-    csv_reader = csv.DictReader(csv_file)
-    for line in csv_reader:
-        data_list.append(line)
-
-result_list = []
-for x in data_list:
-    result_list.append([x['Overall rank'], x['Country or region'], x['GDP per capita']])
-print(sort_value(result_list))
+# import csv
 #
-result_list = []
-top_string = ''
-top_list = []
-for x in data_list:
-    top_list.append(x['GDP per capita'])
-while len(result_list) < 10:
-    for x in data_list:
-        if x['GDP per capita'] == max(top_list) and x not in result_list:
-            result_list.append(x)
-            top_list.remove(x['GDP per capita'])
+# with open('csv_files/2019.csv', 'r', encoding='utf8') as file:
+#     csv_reader = list(csv.DictReader(file))
+#     # print(csv_reader)
 #
-# for res in result_list:
-#     print(res)
+# def sort_values(val):
+#     return val['GDP per capita']
+#
+# csv_reader.sort(key=sort_values, reverse=True)
+#
+# for line in csv_reader:
+#     print(line)
+
+# import csv
+#
+# with open('csv_files/2019.csv', 'r', encoding='utf8') as file:
+#     csv_reader = list(csv.DictReader(file))
+#     # print(csv_reader)
+#
+# csv_reader.sort(key=lambda val: val['GDP per capita'], reverse=True)
+#
+# for line in csv_reader:
+#     print(line)
+
+# x = lambda num1, num2: num1 ** num2
+# print(x(10, 2))
+# print(x(5, 3))
 
 
-# code_list = ['124365432-002', '124361231-004', '124361368-001']
-# print(sorted(code_list, key=lambda x: x[-3:]))
+# sample = [(3, 6, 6), (1, 8, 9), (2, 7, 3)]
+# sample.sort()
+# print(sample)
+# sample.sort(key=lambda x: x[1])
+# print(sample)
+# sample.sort(key=lambda x: x[2])
+# print(sample)
+#
+# def sort_vals(x):
+#     return x[2]
